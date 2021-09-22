49c031f15c43d8d5bba58a65625aa49d655fbfe3
Lucas Matusalem <lucas@matusa.dev>
2021-09-22 07:53:34 -0300

    add fauna db and user login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Provider as NextAuthProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 
@@ -5,10 +6,10 @@ import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
   )    
 }
 
